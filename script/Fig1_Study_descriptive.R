aa19bfc5b35e5fe358c83d5d769bbf813b222f41
Deus <d.thindwa@imperial.ac.uk>
2022-01-15 13:59:12 +0200

    mlw

--- script/Fig1_Study_descriptive.R
+++ script/Fig1_Study_descriptive.R
@@ -47,7 +47,20 @@ C <-  pcvpa.des %>% group_by(nochild5, serogroup) %>% tally() %>%
   ggplot(mapping = aes(x = nochild5, y = serogroup, fill = n)) +
   geom_point(aes(size = n, color = n), shape = 21) +
   theme_bw() +
-  labs(title = "C", x = "Number of children <5 years-old in the house", y = "Serogroup in HIV-infected adults") +
+  labs(title = "C", x = "# of <5y children in the house", y = "Serogroup in HIV-infected adults") +
+  scale_fill_continuous(type = "viridis") +
+  scale_color_continuous(type = "viridis") +  
+  theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
+  theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size=12), axis.title.y = element_text(face = "bold", size = 12)) +
+  guides(fill = guide_legend(title = ""), size = guide_legend(title = "")) + 
+  theme(legend.position = "bottom")
+
+#pneumococcal serogroup and year of survey
+D <-  pcvpa.des %>% group_by(as.integer(year(ymd(pcvpa.des$date))), serogroup) %>% tally() %>%
+  ggplot(mapping = aes(x = `as.integer(year(ymd(pcvpa.des$date)))`, y = serogroup, fill = n)) +
+  geom_point(aes(size = n, color = n), shape = 21) +
+  theme_bw() +
+  labs(title = "D", x = "Year of survey", y = "Serogroup in HIV-infected adults") +
   scale_fill_continuous(type = "viridis") +
   scale_color_continuous(type = "viridis") +  
   theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
@@ -56,30 +69,30 @@ C <-  pcvpa.des %>% group_by(nochild5, serogroup) %>% tally() %>%
   theme(legend.position = "bottom")
 
 #pneumococcal serogroup and age
-D <- ggplot(data = pcvpa.des, mapping = aes(x = serogroup, y = age, fill=serogroup)) + 
+E <- ggplot(data = pcvpa.des, mapping = aes(x = serogroup, y = age, fill=serogroup)) + 
   geom_boxplot(notch = TRUE, color = "black", size = 1) + 
   theme_bw() +
-  labs(title = "D", x = "Serogroup", "Age (years)") +
+  labs(title = "E", x = "Serogroup", "Age (years)") +
   scale_y_continuous(breaks = seq(0, 40, 4)) +
   theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
   theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size = 12), axis.title.y = element_text(face = "bold", size = 12)) +
   theme(legend.position = "none")
 
 #pneumococcal serogroup and ART duration
-E <- ggplot(data = pcvpa.des, mapping = aes(x = serogroup, y = artdur, fill = serogroup)) + 
+F <- ggplot(data = pcvpa.des, mapping = aes(x = serogroup, y = artdur, fill = serogroup)) + 
   geom_boxplot(notch = TRUE, color = "black", size = 1) + 
   theme_bw() +
-  labs(title = "E", x = "Serogroup", y = "ART duration (years)") +
+  labs(title = "F", x = "Serogroup", y = "ART duration (years)") +
   scale_y_continuous(breaks = seq(0, 18, 3)) +
   theme(axis.text.x = element_text(face = "bold", size = 12),axis.text.y = element_text(face = "bold", size = 12)) +
   theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size = 12), axis.title.y = element_text(face = "bold", size = 12)) +
   theme(legend.position = "none")
 
 #pneumococcal serogroup and CD4+ cell count
-F <- ggplot(data = pcvpa.des, mapping = aes(x = serogroup, y = cd4cnt, fill = serogroup)) + 
+G <- ggplot(data = pcvpa.des, mapping = aes(x = serogroup, y = cd4cnt, fill = serogroup)) + 
   geom_boxplot(notch = TRUE, color = "black", size = 1) +
   theme_bw() +
-  labs(title = "F", x = "Serogroup", y = "CD4+ count (cells/mm3)") +
+  labs(title = "G", x = "Serogroup", y = "CD4+ count (cells/mm3)") +
   scale_y_continuous(breaks = seq(0, 1400, 200)) +
   theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
   theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size = 12), axis.title.y = element_text(face = "bold", size = 12)) +
@@ -87,55 +100,55 @@ F <- ggplot(data = pcvpa.des, mapping = aes(x = serogroup, y = cd4cnt, fill = se
 
 #sex
 filter(pcvpa.des, !is.na(sex)) %>% group_by(sex, serogroup) %>% tally() %>% mutate(sex_p = n/sum(n))
-G <- filter(pcvpa.des, !is.na(sex)) %>% group_by(sex, serogroup) %>% tally() %>% mutate(sg_p = n/sum(n)) %>% 
+H <- filter(pcvpa.des, !is.na(sex)) %>% group_by(sex, serogroup) %>% tally() %>% mutate(sg_p = n/sum(n)) %>% 
   ggplot(mapping = aes(x = sex, y = sg_p, fill = serogroup)) + 
   geom_bar(stat = "identity", color = "black", size = 1) +
   scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9")) +
   theme_bw() +
-  labs(title = "G", x = "", y = "Proportion in sex groups") +
+  labs(title = "H", x = "", y = "Proportion in sex groups") +
   scale_y_continuous(breaks = seq(0, 1, 0.2), labels = scales::percent_format(accuracy = 1)) +  theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
   theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size = 12), axis.title.y = element_text(face = "bold", size = 12)) +
   theme(legend.position = "bottom", legend.title = element_blank(), legend.text = element_text(face = "bold", size = 12), legend.box.spacing = unit(0, 'cm'))
 
 #social economic status
 filter(pcvpa.des, !is.na(sescat)) %>% group_by(sescat, serogroup) %>% tally() %>% mutate(ses_p = n/sum(n))
-H <- filter(pcvpa.des, !is.na(sescat)) %>% group_by(sescat, serogroup) %>% tally() %>% mutate(sg_p = n/sum(n)) %>%
+I <- filter(pcvpa.des, !is.na(sescat)) %>% group_by(sescat, serogroup) %>% tally() %>% mutate(sg_p = n/sum(n)) %>%
   ggplot(mapping = aes(x = sescat, y = sg_p, fill = serogroup)) + 
   geom_bar(stat = "identity", color = "black", size = 1) +  
   scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9")) +
   theme_bw() +
-  labs(title = "H", x = "", y = "Proportion in social economic status") +
+  labs(title = "I", x = "", y = "Proportion in social economic status") +
   scale_y_continuous(breaks = seq(0, 1, 0.2), labels = scales::percent_format(accuracy = 1)) +  theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
   theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size = 12), axis.title.y = element_text(face = "bold", size = 12)) +
   theme(legend.position = "bottom", legend.title = element_blank(), legend.text = element_text(face = "bold", size = 12), legend.box.spacing = unit(0, 'cm'))
 
 #ART regimen
 filter(pcvpa.des, !is.na(artreg)) %>% group_by(artreg, serogroup) %>% tally() %>% mutate(artreg_p = n/sum(n))
-I <- filter(pcvpa.des, !is.na(artreg)) %>% group_by(artreg, serogroup) %>% tally() %>% mutate(sg = n/sum(n)) %>%
+J <- filter(pcvpa.des, !is.na(artreg)) %>% group_by(artreg, serogroup) %>% tally() %>% mutate(sg = n/sum(n)) %>%
   ggplot(mapping = aes(x = artreg, y = sg, fill = serogroup)) + 
   geom_bar(stat = "identity", color = "black", size = 1) +  
   scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9")) +
   theme_bw() +
-  labs(title = "I", x = "", y = "Proportion in ART regimen") +
+  labs(title = "J", x = "", y = "Proportion in ART regimen") +
   scale_y_continuous(breaks = seq(0, 1, 0.2), labels = scales::percent_format(accuracy = 1)) +  theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
   theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size = 12), axis.title.y = element_text(face = "bold", size = 12)) +
   theme(legend.position = "bottom", legend.title = element_blank(), legend.text = element_text(face = "bold", size = 12), legend.box.spacing = unit(0, 'cm'))
 
 #cotrimoxazole
 filter(pcvpa.des, !is.na(ctx)) %>% group_by(ctx, serogroup) %>% tally() %>% mutate(ctx_p = n/sum(n))
-J <- filter(pcvpa.des, !is.na(ctx)) %>% group_by(ctx, serogroup) %>% tally() %>% mutate(sg = n/sum(n)) %>%
+K <- filter(pcvpa.des, !is.na(ctx)) %>% group_by(ctx, serogroup) %>% tally() %>% mutate(sg = n/sum(n)) %>%
   ggplot(mapping = aes(x = ctx, y = sg, fill = serogroup)) + 
   geom_bar(stat = "identity", color = "black", size = 1) +  
   scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9")) +
   theme_bw() +
-  labs(title = "J", x = "", y = "Proportion in cotrimoxazole status") +
+  labs(title = "K", x = "", y = "Proportion in cotrimoxazole status") +
   scale_y_continuous(breaks = seq(0, 1, 0.2), labels = scales::percent_format(accuracy = 1)) +  theme(axis.text.x = element_text(face = "bold", size = 12), axis.text.y = element_text(face = "bold", size = 12)) +
   theme(plot.title = element_text(size = 22), axis.title.x = element_text(face = "bold", size = 12), axis.title.y = element_text(face = "bold", size = 12)) +
   theme(legend.position = "bottom", legend.title = element_blank(), legend.text = element_text(face = "bold", size = 12), legend.box.spacing = unit(0, 'cm'))
 
 #combined plots
 ggsave(here("output", "Fig1_descriptive.png"),
-       plot = (A + inset_element(X, right = 0.7, bottom = 0.4, left = 0.3, top = 0.9) + B + C) / (D | E | F | G | H | I | J),
+       plot = (A + inset_element(X, right = 0.7, bottom = 0.4, left = 0.3, top = 0.9) + B + C + D) / (E | F | G | H | I | J | K),
        width = 22, height = 13, unit="in", dpi = 250)
 
 #=======================================================================
